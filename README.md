a345a5714ff24d038d5075c5543e6b38a195996d
Sethuselvi <sethuselvi-98@gmail.com>
2026-03-07 21:02:24 +0530

    Adeed login feature .Installed axios and cors middleware in backend and used for frontend and backend integration for login page

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Install Tailwind css
 Install Desi UI
 Add NavBar component App.jsx
 Installed react-router
+Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+Create an Outlet in your Body Component
+Create a footer
+Create a Login Page
+Install axios
+CORS - install cors in backend => add middleware to with configurations: orgin, credentials: true
+Whenever you're making API call so pass axios => { withCredentials: true }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 const Login = () => {
+
+  const [emailId,setemailId]= useState("")
+  const [password,setpassword] =useState("")
+
+  const handleLogin = async ()=>{
+    try{
+    const res = await axios.post("http://localhost:3000/login",{emailId,password},{withCredentials:true})
+    }catch(err){
+      console.error(err);
+    }
+  }
   return (
-    <div>Login</div>
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-base-300 w-96">
+  <div className="card-body ">
+    <h2 className="card-title justify-center">Login</h2>
+   <div>
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">Email ID</legend>
+  <input type="text" className="input" value={emailId} onChange={(e)=>{setemailId(e.target.value)}}/>
+  <legend className="fieldset-legend">Password</legend>
+  <input type="password" className="input" value={password} onChange={(e)=>{setpassword(e.target.value)}}/>
+</fieldset>
+   </div>
+    <div className="card-actions justify-center  my-4">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
+    </div>
+   
   )
 }
 
