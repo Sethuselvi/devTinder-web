61b2c5ece340845afc18210c49bea61888c00998
Sethuselvi <sethuselvi-98@gmail.com>
2026-03-13 12:11:09 +0530

    Added Feed response in frontend and added static user card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
+import {useEffect} from 'react'
+import axios from 'axios'
+import {BASE_URL} from '../utils/constants'
+import { addFeed } from '../utils/feedSlice'
+import {useDispatch,useSelector} from 'react-redux'
+import UserCard from './UserCard'
 const Feed = () => {
+   const dispatch = useDispatch();
+   //Reading feed
+    const feed = useSelector((store)=>store.feed)
+  const getFeed = async ()=>{
+    
+    if(feed) return
+    try{
+    const res = await axios.get(BASE_URL + "/feed",{withCredentials:true});
+    console.log(res)
+
+    //Using feed slice
+    dispatch(addFeed(res?.data?.data))
+    }catch(err){
+    console.error(err);
+    }    
+  }
+  useEffect(()=>{
+    getFeed();
+  },[])
   return (
-    <div>Feed</div>
+    <UserCard/>
   )
 }
 
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from './userSlice';
+import feedReduer from './feedSlice'
 const appStore = configureStore({
 //Add this slice to appStore
 reducer:{
 user:useReducer,
+feed:feedReduer,
 }
 
 })
