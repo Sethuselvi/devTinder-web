7fa551ad13b48632e7c84fd8264bd8df96081906
Sethuselvi <sethuselvi-98@gmail.com>
2026-03-17 13:52:14 +0530

    Added buttons for accept and reject .Added removeUser logic in requestUser slice to remove the connection if it is accepted or rejected

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Logout Feature
 Edit Profile Feature
 Show Toast Message on save of profile
 New Page - See all my connections
+New Page - See all my connections
+New Page - See all my Conenction REquests
+Feature - Accept/Reject connection request
--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,5 +1,5 @@
 import {useEffect} from 'react'
-import { addRequests } from '../utils/requestSlice'
+import { addRequests,removeRequest } from '../utils/requestSlice'
 import { useDispatch ,useSelector} from 'react-redux'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
@@ -16,16 +16,24 @@ const Requests = () => {
            console.error(err); 
         }
     }
+    const reviewRequest = async (status,_id)=>{
+      try{
+        const res = await axios.post(BASE_URL + '/request/review/' + status +"/" +_id,{},{withCredentials:true})
+        dispatch(removeRequest(_id))
+      }catch(err){
+        console.error(err)
+      }
+    }
     useEffect(()=>{fetchConnections()},[])
     if(!requests) return
-    if(requests.length==0) return <h1> No connections </h1>
+    if(requests.length==0) return <h1 class="flex justify-center my-10"> No connections </h1>
   return (
     <div className='text-center my-10'>
       <h1 className='text-bold text-3xl'>Connections</h1>
       {requests.map((request)=>{
         const {_id,firstName,lastName,photoUrl,age,gender,about}=request.fromUserId;
         return (
-          <div key={_id} className="flex flex-col m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div key={_id} className="flex flex-col m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto">
             <div>
               <img src={photoUrl} alt="photo" className="w-10 h-10 rounded-full"/>
             </div>
@@ -35,6 +43,10 @@ const Requests = () => {
               </h2>
              {age && gender && <p>{age + ", " + gender}</p>}
              <p>{about}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary mx-2" onClick={()=>reviewRequest("rejected",request._id)}>Reject</button>
+              <button className="btn btn-secondary mx-2" onClick={()=>reviewRequest("accepted",request._id)}>Accept</button>
             </div>
           </div>
         )
