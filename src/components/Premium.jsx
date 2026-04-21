b5b5f260e04000cff34a73313963cfc97a1b1722
Sethuselvi <sethuselvi-98@gmail.com>
2026-04-21 18:51:05 +0530

    Added Payment dialog box popup configuration

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,6 +1,34 @@
+import axios from 'axios'
 import React from 'react'
+import { BASE_URL } from '../utils/constants'
 
 const Premium = () => {
+  const handleBuyClick = async (type) =>{
+    const order = await axios.post(BASE_URL + "/payment/create" ,{membershipType:type},{withCredentials:true})
+    //It should open Razor pay dialog box
+    const {amount,keyId,currency,notes,orderId} = order.data
+     const options = {
+        key: keyId,
+        amount, // Amount is in currency subunits.
+        currency,
+        name: 'Dev Tinder',
+        description: 'Connect to other developers',
+        order_id: orderId,
+        prefill: {
+          name: notes.firstName + " " + notes.lastName,
+          email: notes.emailId,
+          contact: '9999999999'
+        },
+        
+        theme: {
+          color: '#F37254'
+        },
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    }
+
   return (
     <div className="m-10">
     <div className="flex w-full">
@@ -12,7 +40,7 @@ const Premium = () => {
         <li>- Blue Tick</li>
         <li>- 3 months</li>   
     </ul>
-    <button className="btn btn-secondary">Buy Now</button>
+    <button onClick= {()=>handleBuyClick("silver")} className="btn btn-secondary">Buy Now</button>
     </div>
   <div className="divider divider-horizontal">OR</div>
   <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -23,7 +51,7 @@ const Premium = () => {
         <li>- Blue Tick</li>
         <li>- 6 months</li>   
     </ul>
-    <button className="btn btn-primary">Buy Now</button>
+    <button onClick= {()=>handleBuyClick("gold")} className="btn btn-primary">Buy Now</button>
     </div>
 </div>
 </div>
